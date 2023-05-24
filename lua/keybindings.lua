7304770293a585dfa522c0a8038e3e2d5df34c56
Yttrium_32 <shresthprasad7@gmail.com>
2023-05-24 18:50:59 +0530

    Temporarily remove quick build and snippet keybinds

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,14 +1,6 @@
 vim.g.mapleader = " "
 local keymap = vim.api.nvim_set_keymap
 
-local function setup_make_file ()
-  print("hello")
-end
-
-local function build_and_run ()
-  print("hello")
-end
-
 keymap('n', '<F2>', ':NeoTreeRevealToggle<CR>', {silent=true})
 keymap('n', '<C-e>', '$', {})
 keymap('n', '<C-a>', '0', {})
@@ -40,28 +32,4 @@ keymap('n', 'f', ':BufferLinePick<CR>', {})
 -- Save all open buffers
 keymap('n', '<C-s>', ':wa<CR>', {})
 keymap('i', '<C-s>', ':wa<CR>', {})
-
--- Easy C execution and setup
-keymap('n', '<F9>', '<cmd>lua build_and_run<CR>', {})
-keymap('n', '<F12>', '<cmd>lua setup_make_file<CR>', {})
-
---" Use <C-l> for trigger snippet expand.
-keymap('i', '<C-l>', '<Plug>(coc-snippet-expand)', {})
-
--- Use <C-j> for select text for visual placeholder of snippet.
-keymap('v', '<C-j>', '<Plug>(coc-snippet-select)', {})
-
--- Use <C-j> for jump to next placeholder, it's default of coc.nvim
-vim.g.coc_snippet_next = '<c-j>'
-
--- Use <C-k> for jump to previous placeholder, it's default of coc.nvim
-vim.g.coc_snippet_prev = '<c-k>'
-
--- Use <C-j> for both expand and jump (make expand higher priority.)
---imap <C-j> <Plug>(coc-snippets-expand-jump)
-keymap('i', '<C-j>', '<Plug>(coc-snippet-expand-jump)', {})
-
--- Use <leader>x for convert visual selected code to snippet
---xmap <leader>x  <Plug>(coc-convert-snippet)
-keymap('x', '<Leader>x', '<Plug>(coc-convert-snippet)', {})
 
