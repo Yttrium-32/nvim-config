3f7d5561d5ebca90e9ce7affdad372cdf85fc476
Yttrium_32 <shresthprasad7@gmail.com>
2023-11-03 12:45:02 +0530

    Update indent blank line config

--- init.lua
+++ init.lua
@@ -30,11 +30,7 @@ vim.opt.clipboard:append("unnamedplus")
 -- Indent guide config
 vim.opt.list = true
 
-require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-}
+require("ibl").setup()
 
 -- Treesitter config
 require('nvim-treesitter.configs').setup {
