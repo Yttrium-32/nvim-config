1eb80c28d5f2c9b4f788a23ec364d244c9f02634
Shresth Prasad <shresthprasad7@gmail.com>
2024-04-04 16:42:44 +0530

    add lsp plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,4 +22,24 @@ return require('packer').startup(function(use)
 	  end,
   }
 
+  -- Tree view for undo
+  use 'mbbill/undotree'
+
+  -- all plugins for lsp configuration
+  use {
+	  'neovim/nvim-lspconfig',
+	  requires = {
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'hrsh7th/cmp-cmdline'},
+		  {'hrsh7th/nvim-cmp'},
+		  {'L3MON4D3/LuaSnip'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'j-hui/fidget.nvim'},
+	  }
+  }
+
   end)
