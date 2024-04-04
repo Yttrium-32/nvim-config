df1f1bb7aed42888d1471e53d53008f3cd9a527d
Shresth Prasad <shresthprasad7@gmail.com>
2024-04-04 18:52:34 +0530

    change structure of keybinds

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,40 +1,41 @@
+-- Some local variables for ease of use
+local augroup = vim.api.nvim_create_augroup
+local LspBindGroup = augroup('ThePrimeagen', {})
+local autocmd = vim.api.nvim_create_autocmd
+local builtin = require('telescope.builtin')
+local keymap = vim.keymap.set
+
 -- Bind leader key to spacebar
 vim.g.mapleader = " "
 
 -- Open filetree with F2
-vim.keymap.set("n", "<F2>", vim.cmd.Ex)
+keymap("n", "<F2>", vim.cmd.Ex)
 
 -- Telescope keybinds
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+keymap('n', '<leader>pf', builtin.find_files, {})
+keymap('n', '<C-p>', builtin.git_files, {})
+keymap('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
 -- Open undotree
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+keymap('n', '<leader>u', vim.cmd.UndotreeToggle)
 
 -- LSP specific keybinds
-local augroup = vim.api.nvim_create_augroup
-local LspBindGroup = augroup('ThePrimeagen', {})
-local autocmd = vim.api.nvim_create_autocmd
-
 autocmd('LspAttach', {
     group = LspBindGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        keymap("n", "K", function() vim.lsp.buf.hover() end, opts)
+        keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        keymap("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        keymap("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+        keymap("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+        keymap("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        keymap("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        keymap("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
 
