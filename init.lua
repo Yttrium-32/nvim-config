dcb3766e21c9bab523cc338cee599f00ecb0d102
Shresth Prasad <shresthprasad7@gmail.com>
2024-04-04 18:44:58 +0530

    add keybind for lsp

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
-require("keybinds")
 require("plugins")
+require("keybinds")
+
+require("plugin-configs.treesitter")
+require("plugin-configs.lsp")
 
 -- Set current colorscheme to onedark
 vim.cmd("colorscheme onedark")
 
 -- Make the background transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })                                                                                                                                              
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
 
