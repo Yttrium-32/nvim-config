e8548fc7681d044a125a01e8e2de996e3df5ccc0
Shresth Prasad <shresthprasad7@gmail.com>
2024-04-04 15:44:08 +0530

    add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,4 +13,13 @@ return require('packer').startup(function(use)
   -- One dark color scheme
   use 'joshdick/onedark.vim'
 
+  -- Treesitter for highlights
+  use {
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		  ts_update()
+	  end,
+  }
+
   end)
