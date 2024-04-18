572daaad6c9332e42bb159bb01e64b7973090fb4
Shresth Prasad <shresthprasad7@gmail.com>
2024-04-18 15:22:14 +0530

    remove redundant require statement

--- lua/plugin-configs/lsp.lua
+++ lua/plugin-configs/lsp.lua
@@ -14,7 +14,6 @@ local capabilities = vim.tbl_deep_extend(
 vim.lsp.protocol.make_client_capabilities(),
 cmp_lsp.default_capabilities())
 
-require("fidget").setup({})
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = {
