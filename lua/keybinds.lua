5e93398bbc528b163e1e56a5e58ae8285550eec0
Shresth Prasad <shresthprasad7@gmail.com>
2025-07-15 15:14:13 +0530

    rename border variable

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -34,7 +34,7 @@ autocmd('LspAttach', {
         local opts = { buffer = e.buf }
         keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
         keymap("n", "K", function() vim.lsp.buf.hover({
-            border = _BORDER
+            border = _Border
         }) end, opts)
         keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -45,13 +45,13 @@ autocmd('LspAttach', {
         keymap("n", "]d", function() vim.diagnostic.jump({
             count = 1,
             float = {
-                border = _BORDER
+                border = _Border
             }
         }) end, opts)
         keymap("n", "[d", function() vim.diagnostic.jump({
             count = -1,
             float = {
-                border = _BORDER
+                border = _Border
             }
         }) end, opts)
     end
