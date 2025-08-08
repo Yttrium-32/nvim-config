190cab24b37eb78fa0b4beda1039a70eaaf126a0
Shresth Prasad <shresthprasad7@gmail.com>
2025-08-08 23:59:54 +0530

    Convert " to ' in plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out,                            'WarningMsg' },
+            { '\nPress any key to exit...' },
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
@@ -16,7 +16,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup {
+require('lazy').setup {
     spec = {
         -- One dark color scheme
         {
@@ -24,7 +24,7 @@ require("lazy").setup {
             lazy = false,
             priority = 1000,
             config = function()
-                vim.cmd("colorscheme onedark")
+                vim.cmd('colorscheme onedark')
             end,
         },
 
@@ -60,11 +60,11 @@ require("lazy").setup {
 
         -- Code snippets
         {
-            "L3MON4D3/LuaSnip",
+            'L3MON4D3/LuaSnip',
             -- follow latest release.
-            version = "v2.*",
+            version = 'v2.*',
             -- install jsregexp (optional!).
-            build = "make install_jsregexp",
+            build = 'make install_jsregexp',
             -- Code snippet source
             dependencies = { 'rafamadriz/friendly-snippets' }
         },
@@ -80,13 +80,13 @@ require("lazy").setup {
 
         -- A sane file tree
         {
-            "nvim-neo-tree/neo-tree.nvim",
+            'nvim-neo-tree/neo-tree.nvim',
             branch = "v3.x",
             dependencies = {
-                "nvim-lua/plenary.nvim",
-                "nvim-tree/nvim-web-devicons",
-                "MunifTanjim/nui.nvim",
-            }
+                'nvim-lua/plenary.nvim',
+                'nvim-tree/nvim-web-devicons',
+                'MunifTanjim/nui.nvim',
+            },
         },
 
         -- Status line
@@ -115,9 +115,9 @@ require("lazy").setup {
 
         -- Prettier diagnostics
         {
-            "folke/trouble.nvim",
+            'folke/trouble.nvim',
             opts = {}, -- for default options
-            cmd = "Trouble",
+            cmd = 'Trouble',
         },
 
         -- For better git integration
@@ -132,7 +132,7 @@ require("lazy").setup {
     },
     -- Configure any other settings here. See the documentation for more details.
     -- color scheme that will be used when installing plugins.
-    install = { colorscheme = { "onedark" } },
+    install = { colorscheme = { 'onedark' } },
     -- automatically check for plugin updates
     checker = { enabled = true },
     ui = {
