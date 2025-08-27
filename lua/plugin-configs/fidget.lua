50274127dec4f6844c59debc8fe68a7ca6ac09d8
Shresth Prasad <shresthprasad7@gmail.com>
2025-08-27 21:52:39 +0530

    change fidget.nvim highlights

--- lua/plugin-configs/fidget.lua
+++ lua/plugin-configs/fidget.lua
@@ -1,6 +1,10 @@
 require("fidget").setup({
     notification = {
+        view = {
+            group_separator_hl = "NormalNC"
+        },
         window = {
+            normal_hl = "NormalNC",
             winblend = 0,
             relative = "editor",
             align = "bottom"
