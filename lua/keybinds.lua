ec4df536e0ef06bfe9152f9631a5e41cc4cf74d2
Shresth Prasad <shresthprasad7@gmail.com>
2025-08-29 21:16:19 +0530

    remove border options from keybinds

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -46,9 +46,6 @@ autocmd('LspAttach', {
         keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
         keymap("n", "gi", function() vim.lsp.buf.implementation() end, opts)
         keymap("n", "K", function()
-            vim.lsp.buf.hover({
-                border = _Border
-            })
         end, opts)
         keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -59,17 +56,11 @@ autocmd('LspAttach', {
         keymap("n", "]d", function()
             vim.diagnostic.jump({
                 count = 1,
-                float = {
-                    border = _Border
-                }
             })
         end, opts)
         keymap("n", "[d", function()
             vim.diagnostic.jump({
                 count = -1,
-                float = {
-                    border = _Border
-                }
             })
         end, opts)
     end
